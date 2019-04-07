a5520b5a784769a8daa6d5cd826e985e9866239d
youyo <1003ni2@gmail.com>
2019-04-08 06:15:52 +0900

    update

--- task1.sh
+++ task1.sh
@@ -7,7 +7,7 @@ run_task(){
 	aws ecs run-task \
 		--cluster agent-fargate-cluster \
 		--task-definition agent-batch-export-activity-salesforce \
-		--overrides "{\"name\":\"agent-batch-export-activity-salesforce\",\"environment\":[{\"name\":\"INPUT\",\"value\":\"$INPUT_OVERRIDE\"}]}" \
+		--overrides "{\"containerOverrides\":[{\"name\":\"agent-batch-export-activity-salesforce\",\"environment\":[{\"name\":\"INPUT\",\"value\":\"$INPUT_OVERRIDE\"}]}]}" \
 		--count 1 \
 		--launch-type FARGATE \
 		--network-configuration "{\"awsvpcConfiguration\":{\"subnets\":[\"subnet-0168d996eef694152\",\"subnet-0bd2e260bf2586d1f\",\"subnet-091381f22201a84bb\"],\"securityGroups\":[\"sg-053c9d616dfd01730\"],\"assignPublicIp\":\"ENABLED\"}}"
